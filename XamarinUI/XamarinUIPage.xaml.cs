2d069520fd8b256670b75361d60fababe3dafca6
andrea.calegari <andrea@nearit.com>
2017-12-19 10:15:10 +0100

    First project commit

---
+++ XamarinUI/XamarinUIPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinUI
+{
+    public partial class XamarinUIPage : ContentPage
+    {
+        public XamarinUIPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
